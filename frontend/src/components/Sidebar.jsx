7ac1484c65d71b41299f552c0c1ee28a197d90f1
nidhipawar5 <nidhipawar0504@gmail.com>
2024-05-04 20:33:05 +0530

    SignUpPage component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -11,10 +11,10 @@ import Logout from "./Logout"
 
 const Sidebar = () => {
 
-  const authUser = true;
+  const authUser = false;
 
   return (
-    <aside className='flex flex-col items-center min-w-12  sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 hover:bg-gray-600/10 border-gray-800 text-white'>
+    <aside className='flex flex-col items-center min-w-12  sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-glass'>
       <nav className='h-full flex flex-col gap-3'>
         <Link to='/' className='flex justify-center'>
           <img className='h-8' src='/github.svg' alt='Github Logo' />
