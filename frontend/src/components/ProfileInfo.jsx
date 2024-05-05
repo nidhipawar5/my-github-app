603aaf23a1f3a891accb2699cd3b79005f39112c
nidhipawar5 <nidhipawar0504@gmail.com>
2024-05-05 11:55:26 +0530

    Home page with search and sort repos UI

---
+++ frontend/src/components/ProfileInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileInfo = () => {
+  return (
+    <div>ProfileInfo</div>
+  )
+}
+
+export default ProfileInfo
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import Search from '../components/Search';
+import SortRepos from '../components/SortRepos';
+import ProfileInfo from '../components/ProfileInfo';
+import Repos from '../components/Repos';
+import Spinner from '../components/Spinner';
+
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+	return (
+		<div className='m-4'>
+			<Search />
+			<SortRepos />
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				<ProfileInfo />
+				<Repos />
+				<Spinner />
+			</div>
+		</div>
+	);
+};
 
 export default HomePage
