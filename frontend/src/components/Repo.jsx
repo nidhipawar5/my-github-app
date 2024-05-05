2bccf96d83951f103b416c1f1559c084a6f4fec0
nidhipawar5 <nidhipawar0504@gmail.com>
2024-05-05 12:05:04 +0530

    Repos and repo component added

---
+++ frontend/src/components/Repo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Repo = () => {
+  return (
+    <div>Repo</div>
+  )
+}
+
+export default Repo
--- frontend/src/components/Repos.jsx
+++ frontend/src/components/Repos.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Repo from "./Repo";
+
 const Repos = () => {
-  return (
-    <div>Repos</div>
-  )
-}
+	return (
+		<div className={`lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6`}>
+			<ol className='relative border-s border-gray-200'>
+				<Repo />
+				<Repo />
+				<Repo />
+			</ol>
+		</div>
+	);
+};
 
 export default Repos
