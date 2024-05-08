315792fb6d535d1c5f48cee32f52272f778e970c
nidhipawar5 <nidhipawar0504@gmail.com>
2024-05-08 19:28:28 +0530

    callback url change in github auth

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -22,7 +22,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: "/api/auth/github/callback",
+			callbackURL: "https://my-github-app.onrender.com/api/auth/github/callback",
 		},
 		async function (accessToken, refreshToken, profile, done) {
             //console.log(profile)
