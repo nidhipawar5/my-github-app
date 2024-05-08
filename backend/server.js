b037e30dbf7e7b5eaf7579eba6d3df4331572600
nidhipawar5 <nidhipawar0504@gmail.com>
2024-05-08 18:56:05 +0530

    readying for deployment, on master

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import passport from 'passport'
 import session from 'express-session'
+import path from 'path'
 
 import "./passport/github.auth.js"
 
@@ -15,6 +16,11 @@ import connectMongoDB from './db/connectMongoDB.js'
 dotenv.config()
 
 const app =express()
+
+const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
+//console.log("dirname",__dirname)
+
 app.use(cors())
 
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
@@ -25,15 +31,17 @@ app.use(passport.session());
 
 
 
-app.get("/", (req,res) => {
-    res.send("Server is ready")
-})
-
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/explore", exploreRoutes)
 
-app.listen(5001, () => {
-    console.log("Server started on http://localhost:5001");
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
     connectMongoDB()
 })
